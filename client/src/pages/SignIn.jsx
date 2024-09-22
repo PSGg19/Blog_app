a24be054f978951d6c98308d75c3a326a21806f5
PSGg19 <prahladsharan19@gmail.com>
2024-09-22 10:00:00 +0530

    Minor bugs and error solved in sign in

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import { TextInput, Label, Button, Alert, Spinner } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import {
   signInFailure,
   signInStart,
   signInSuccess,
 } from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 import Oauth from "../components/Oauth";
 
 export default function SignIn() {
-  const [formData, setFormdata] = useState({ email: "", password: "" });
-  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const [formData, setFormdata] = useState({});
+  const {loading, error:errorMessage } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormdata({ ...formData, [e.target.id]: e.target.value });
   };
+
+  // console.log(formData);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -45,14 +48,12 @@ export default function SignIn() {
         navigate("/");
       }
     } catch (error) {
-      dispatch(signInFailure(error.message));
-    }
+     dispatch(signInFailure(error.message))  }
   };
-
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-10">
-        {/* Left side */}
+        {/* left div */}
         <div className="w-full flex flex-col justify-center">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
@@ -66,29 +67,25 @@ export default function SignIn() {
           </p>
         </div>
 
-        {/* Right side */}
+        {/* rightdiv */}
         <div className="w-full">
           <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
             <div>
-              <Label value="Your email" />
+              <Label value="your email" />
               <TextInput
-                type="email"
+                type="text"
                 placeholder="Company@gmail.com"
                 id="email"
-                value={formData.email}
                 onChange={handleChange}
-                required
               />
             </div>
             <div>
-              <Label value="Your password" />
+              <Label value="your password" />
               <TextInput
                 type="password"
                 placeholder="********"
                 id="password"
-                value={formData.password}
                 onChange={handleChange}
-                required
               />
             </div>
             <div className="flex flex-col gap-3 mt-4">
@@ -106,11 +103,12 @@ export default function SignIn() {
                   "Sign In"
                 )}
               </Button>
-              <Oauth />
+              <Oauth/>
+
             </div>
           </form>
           <div className="flex gap-2 text-sm mt-5">
-            <span>Don't have an account?</span>
+            <span>don't have an account?</span>
             <Link to="/sign-up" className="text-blue-500">
               Sign Up
             </Link>
